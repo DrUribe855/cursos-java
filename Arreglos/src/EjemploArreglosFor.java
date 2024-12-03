81e69f000e76fd48c8d365df3b9ff76b80699013
Dr.Uribe <yeisonl2036@gmail.com>
2024-12-03 10:58:05 -0500

    Changes

---
+++ Arreglos/src/EjemploArreglosFor.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class EjemploArreglosFor {
+    public static void main(String[] args) {
+
+        String[] productos = new String[7];
+        productos[0] = "Kingstone Pendrive 64GB";
+        productos[1] = "Samsung Galaxy";
+        productos[2] = "Disco duro SSD Samsung externo";
+        productos[3] = "Asus Notebook";
+        productos[4] = "Macbook Air";
+        productos[5] = "Chromecast 4ta generacion";
+        productos[6] = "Bicicleta Oxford";
+
+        Arrays.sort(productos);
+
+        String prod1 = productos[0];
+        String prod2 = productos[1];
+        String prod3 = productos[2];
+        String prod4 = productos[3];
+        String prod5 = productos[4];
+        String prod6 = productos[5];
+        String prod7 = productos[6];
+
+        System.out.println("productos[0] = " + prod1);
+        System.out.println("productos[1] = " + prod2);
+        System.out.println("productos[2] = " + prod3);
+        System.out.println("productos[3] = " + prod4);
+        System.out.println("productos[4] = " + prod5);
+        System.out.println("productos[5] = " + prod6);
+        System.out.println("productos[6] = " + prod7);
+
+
+        int [] numeros = new int[4];
+
+        
+    }
+}
--- HolaMundo/HolaMundo/src/EjemploClaseMathRandom.java
+++ HolaMundo/HolaMundo/src/EjemploClaseMathRandom.java
@@ -1,5 +1,24 @@
+import java.util.Random;
+
 public class EjemploClaseMathRandom {
     public static void main(String[] args) {
-        
+
+        String[] colores = {"azul","amarillo","rojo","verde","blanco","negro"};
+        double random = Math.random();
+        System.out.println("random = " + random);
+        random*= colores.length;
+        System.out.println("random = " + random);
+
+        random = Math.floor(random);
+        System.out.println("random = " + random);
+
+        System.out.println("colores = " + colores[(int)random]);
+
+        Random randomObj = new Random();
+        int randomInt = randomObj.nextInt();
+        System.out.println("randomInt = " + randomInt);
+
+        randomInt = randomObj.nextInt(colores.length);
+
     }
 }
